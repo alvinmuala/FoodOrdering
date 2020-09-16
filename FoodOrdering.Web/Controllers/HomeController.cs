9363a5144e4ee63549197fa633b7b7c0f09b5238
Alvin Muala <Alvin.Muala@sanlam.co.za>
2020-09-16 22:59:47 +0200

    1.delete privacy view  and controller action 2.change application name in main app layout

--- FoodOrdering.Web/Controllers/HomeController.cs
+++ FoodOrdering.Web/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FoodOrdering.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using FoodOrdering.Web.Models;
+using System.Diagnostics;
 
 namespace FoodOrdering.Web.Controllers
 {
@@ -23,12 +19,6 @@ namespace FoodOrdering.Web.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
