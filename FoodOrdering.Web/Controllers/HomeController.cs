ab561b0a6732a1c3863d997242cf1ed4fc73c37f
Alvin Muala <Alvin.Muala@sanlam.co.za>
2020-09-18 02:42:44 +0200

    add logic to process search string and query json data

--- FoodOrdering.Web/Controllers/HomeController.cs
+++ FoodOrdering.Web/Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using FoodOrdering.Web.Models.Restaurant;
 using FoodOrdering.Web.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Diagnostics;
+using System.Linq;
 
 namespace FoodOrdering.Web.Controllers
 {
@@ -20,6 +22,12 @@ namespace FoodOrdering.Web.Controllers
 
         public IActionResult Index()
         {
+            var errorMessage = TempData["ErrorMessage"];
+            if ( errorMessage!= null)
+            {
+                ViewBag.ErrorMessage = errorMessage;
+            }
+            
             return View();
         }
 
@@ -28,13 +36,45 @@ namespace FoodOrdering.Web.Controllers
         {
             if (!string.IsNullOrWhiteSpace(model.SearchString))
             {
-                //process and manipulate string
-                //if string parameters exist in json file then do something else return error 
-                //var restaurants = _restaurantService.GetRestaurants();
+                var searchString = model.SearchString.ToUpperInvariant();
+                if (searchString.Contains("IN"))
+                {
+                    var stringsSplit = searchString.Split(new string[] { "IN" }, StringSplitOptions.None);
+
+                    var menuItem = stringsSplit[0].TrimStart().TrimEnd();
+
+                    var city = stringsSplit[1].TrimStart().TrimEnd();
+
+                    var restaurants = _restaurantService.GetRestaurants();
+                    
+
+                    //todo: add some filtering 
+                    var query = restaurants
+                        .Where(r => r.City.ToUpperInvariant() == city &&
+                            (r.Categories.Any(c => c.Name.ToUpperInvariant().Contains(menuItem) || c.MenuItems.Any(mi => mi.Name.ToUpperInvariant().Contains(menuItem))))
+                        ).ToList();
+
+                    if(query.Count == 0)
+                    {
+                        TempData["ErrorMessage"] = "Ooops! No restaurant is serving what you are looking for at the moment. \n Please try something else. " +
+                            "\n e.g: Taco in Cape Town";
+
+                        RedirectToAction("Index");
+                    }
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Oops! It seems like you have entered some invalid text " +
+                           "\n Please try again by using the format (MenuItem) in (City)." +
+                           " e.g: Taco in Cape Town";
+
+                    return RedirectToAction("Index");
+                }
             }
             else
             {
-                //return error message
+                TempData["ErrorMessage"] = "Oops! Please try again by typing something into the search bar using the format (MenuItem) in (City)" +
+                          "\n e.g: Taco in Cape Town";
             }
             return RedirectToAction("Index");
         }
