7004a42a8fa8b96cc096ff2b60abd0935d8c9871
Alvin Muala <Alvin.Muala@sanlam.co.za>
2020-09-19 15:47:24 +0200

    add logic to sum order total

---
+++ FoodOrdering.Web/wwwroot/js/restaurant.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    $("input[type='checkbox']").change(function () {
+        var cbChecked = new Array();
+
+        $("input[type='checkbox']:checked").each(function () {
+            cbChecked[cbChecked.length] = this.value;
+        });
+
+        if (cbChecked.length == 0) {
+            $("button#cbChecked").html("Order");
+        }
+        else {
+            $("button#cbChecked").html("Order - " + "R" + $.sum(cbChecked));
+        }
+    });
+
+    $.sum = function (arr) {
+        var r = 0;
+        $.each(arr, function (i, v) {
+            r += +v;
+        });
+        return r;
+    }
+});
