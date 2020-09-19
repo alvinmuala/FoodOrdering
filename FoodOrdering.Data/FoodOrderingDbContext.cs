d474fa44fe16693a62f53f148f17321793f94852
Alvin Muala <Alvin.Muala@sanlam.co.za>
2020-09-19 04:10:39 +0200

    add dbContext

---
+++ FoodOrdering.Data/FoodOrderingDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FoodOrdering.Data
+{
+    public class FoodOrderingDbContext : DbContext
+    {
+        public FoodOrderingDbContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+    }
+}
